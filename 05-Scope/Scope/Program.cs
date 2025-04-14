f30630daa16863f9791af4f943326973ab658ed1
majunseob <majunseob@gmail.com>
2025-04-14 17:56:12 +0900

    Scope 생성

---
+++ 05-Scope/Scope/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Scope
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isPlayer = false;
+
+            int age = 30;
+
+            age = OtherScope(age);
+            Console.WriteLine(age);
+        }
+
+        static int OtherScope(int age)
+        {
+            //isPlayer = true; //error
+
+            return ++age;
+        }
+    }
+}
