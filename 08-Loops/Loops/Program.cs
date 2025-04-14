df611781cfbbba5a1b904229c1ee49687ee8cd01
majunseob <majunseob@gmail.com>
2025-04-14 21:33:56 +0900

    Loops 생성

---
+++ 08-Loops/Loops/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ---------- while ---------- //
+            int i = 0;
+            while (i < 3) // 0, 1, 2, 3(out)
+            {
+                Console.WriteLine("Loop..." + i);
+                i++;
+            }
+
+            // ---------- do-while ---------- //
+            // do-while은 "조건이 false여도 한 번은 무조건 실행" 하지만,
+            do
+            {
+                Console.WriteLine("do while Loop..." + i);
+                i++;
+            } while (i < 3);
+
+            // 두 번째부터는 조건을 검사해서 false면 끝난다.
+            i = 0;
+            do
+            {
+                Console.WriteLine("do while Loop..." + i);
+                i++;
+            } while (i < 3);
+
+            // ---------- for ---------- //
+            for (int j = 0; j < 3; j++)
+            {
+                Console.WriteLine("for loop: " + j);
+            }
+
+            string[] nameArray = new string[]
+            {
+                "AAA",
+                "BBB",
+                "CCC",
+                "DDD"
+            };
+            for (int k = 0; k < nameArray.Length; k++) // list라면 count
+            {
+                Console.WriteLine(k + ": " + nameArray[k]);
+            }
+
+            // ---------- foreach ---------- //
+            foreach (string name in nameArray)
+            {
+                Console.WriteLine("foreach: " + name);
+            }
+        }
+    }
+}
