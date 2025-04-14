0dd2e910fac1eef6b08ed694c69afbeea74c398e
majunseob <majunseob@gmail.com>
2025-04-14 14:08:40 +0900

    Variables 생성

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,9 @@
+﻿int age = 25, health = 100, power = 5;
+Console.WriteLine(age);
+
+age = 20 + 1;
+age++;
+age += 1;
+
+Console.WriteLine(age + health + power);
+Console.WriteLine(age + ", " + health + ", " + power);
