26afa7819c2c75c12f3397e8e727f996378f21db
majunseob <majunseob@gmail.com>
2025-04-15 15:16:37 +0900

    CleanCodeGuidelines 생성, learn csharp beginner 완료

---
+++ 12-NamingRules/NamingRules/Program.cs
@@ -0,0 +1,22 @@
+﻿// C# 코드 스타일 가이드 만들기
+// https://unity.com/kr/blog/engine-platform/clean-up-your-code-how-to-create-your-own-c-code-style
+
+// https://github.com/thomasjacobsen-unity/Unity-Code-Style-Guide
+// https://github.com/thomasjacobsen-unity/Unity-Code-Style-Guide/blob/master/StyleExample.cs
+
+// https://unity.com/how-to/naming-and-code-style-tips-c-scripting-unity
+// https://unity.com/how-to/formatting-best-practices-c-scripting-unity
+
+namespace NamingRules
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Spend time deciding on a proper name!
+            // Don't be afraid to rename things (we have a Ctrl+R, R)
+            // Don't use single letter names
+            // Don't use acronyms or abbreviations
+        }
+    }
+}
